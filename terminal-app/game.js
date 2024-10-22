b46623f4883fb3ea74ec75eb927bd0ba84997aa1
ana-konzen <143814540+ana-konzen@users.noreply.github.com>
2024-10-22 10:01:23 -0400

    update

--- terminal-app/game.js
+++ terminal-app/game.js
@@ -63,11 +63,11 @@ const cyan = 0x48d1cc;
 const brown = 0xed9755;
 let doorColor = brown;
 
-const jsonStr = await Deno.readTextFile("game_info.json");
-const gameInfo = JSON.parse(jsonStr);
+// const jsonStr = await Deno.readTextFile("game_info.json");
+// const gameInfo = JSON.parse(jsonStr);
 // const prologue = `${gameInfo.setting}  ${gameInfo.mystery}  ${gameInfo.finalPrize}`;
 
-// const gameInfo = await createGame(numChambers);
+const gameInfo = await createGame(numChambers);
 
 const prologue = await createPrologue(gameInfo, numChambers);
 chatHistory.push(prologue);
